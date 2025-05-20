797c4389601b216584c8b0da64e529cdc7a268e7
anajnsilva <anajnsilva@gmail.com>
2025-05-20 15:05:24 -0700

    Re-added code that got removed from index.js during merge for authorization checking.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fs = require("fs");
 const pg = require("pg");
 const dotenv = require("dotenv").config();
 const notificationUtils = require("./notification-emails");
+const authorization = require("./authorization.js");
 
 const ejs = require("ejs");
 
@@ -97,10 +98,10 @@ app.get("/login", function (req, res) {
 
 // Route for browse page
 app.get("/browse", async function (req, res) {
-    const  { lat, lon } = req.query;
+    const { lat, lon } = req.query;
     const apiKey = process.env.GOOGLE_MAPS_API_KEY;
     const city = await getYourCity(lat, lon, process.env.GOOGLE_MAPS_API_KEY);
-    
+
     res.render("browse", {
         city,
         stylesheets: ["browse.css"],
@@ -119,6 +120,7 @@ app.get("/contents/:id", function (req, res) {
         `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>`,
     ];
     let storageID = req.params.id;
+    let userId = req.session.userId;
     const client = new pg.Client(config);
     client.connect((err) => {
         if (err) {
@@ -127,11 +129,11 @@ app.get("/contents/:id", function (req, res) {
         }
         client.query(
             `
-                    SELECT s."storageType", s."title", s."lastCleaned" 
+                    SELECT s."storageType", s."title", s."lastCleaned", s."ownerId" = $2 AS "isCurrentUserOwner" 
                     FROM public.storage AS s 
                     WHERE s."storageId" = $1`,
-            [storageID],
-            async (error, results) => {
+            [storageID, userId],
+            (error, results) => {
                 if (error) {
                     console.log(error);
                     client.end();
@@ -140,6 +142,7 @@ app.get("/contents/:id", function (req, res) {
                 let type = results.rows[0].storageType;
                 let title = results.rows[0].title;
                 let lastCleaned = results.rows[0].lastCleaned;
+                let authorized = results.rows[0].isCurrentUserOwner;
                 res.render("contents", {
                     type: type,
                     title: title,
@@ -148,6 +151,7 @@ app.get("/contents/:id", function (req, res) {
                     scripts: js,
                     other: other,
                     id: storageID,
+                    auth: authorized,
                 });
                 client.end();
             }
@@ -157,11 +161,15 @@ app.get("/contents/:id", function (req, res) {
 
 app.get("/map/:id", function (req, res) {
     let storageID = req.params.id;
-    res.render("map", {
-        stylesheets: ["contents.css", "contents-modal.css"],
-        scripts: ["locational.js"],
-        id: storageID,
-    });
+    authorization.isAuthorized(storageID, req.session.userId).then(auth => {
+        res.render("map", {
+            stylesheets: ["contents.css", "contents-modal.css"],
+            scripts: ["locational.js"],
+            id: storageID,
+            auth: auth,
+        });
+    })
+
 });
 
 // Route for directions page
@@ -171,42 +179,51 @@ app.get("/directions", function (req, res) {
 });
 
 // Route for manage page
-app.get("/manage/:id", async (req, res) => {
+app.get("/manage/:id", (req, res) => {
     const storageId = req.params.id;
-
-    if (!storageId) {
-        return res.status(400).json({ error: "Storage ID is required" });
-    }
-    const client = new pg.Client(config);
-    try {
-        await client.connect();
-
-        const result = await client.query(
-            `SELECT * FROM public.storage WHERE "storageId" = $1 AND "deletedDate" IS NULL`,
-            [storageId]
-        );
-
-        if (result.rows.length === 0) {
-            return res.status(404).json({ error: "Storage not found" });
+    authorization.isAuthorized(storageId, req.session.userId).then(auth => {
+        if (!auth) {
+            res.redirect("/login");
+            return;
         }
-
-        const storage = result.rows[0];
-
-        if (storage.lastCleaned) {
-            storage.lastCleaned = new Date(storage.lastCleaned);
+        if (!storageId) {
+            return res.status(400).json({ error: "Storage ID is required" });
         }
-        res.render("manage", {
-            storage,
-            stylesheets: ["contents.css", "manage.css"],
-            scripts: ["imageUploadUtil.js", "manage.js"],
-            id: storageId
+        const client = new pg.Client(config);
+        client.connect((err) => {
+            if (err) {
+                console.log(error);
+            }
+            client.query(
+                `SELECT * FROM public.storage WHERE "storageId" = $1 AND "deletedDate" IS NULL`,
+                [storageId], (error, results) => {
+                    client.end();
+                    if (error) {
+                        console.log(error);
+
+                    }
+                    if (results.rows.length === 0) {
+                        return res.status(404).json({ error: "Storage not found" });
+                    }
+                    const storage = results.rows[0];
+
+                    if (storage.lastCleaned) {
+                        storage.lastCleaned = new Date(storage.lastCleaned);
+                    }
+                    res.render("manage", {
+                        storage,
+                        stylesheets: ["contents.css", "manage.css"],
+                        scripts: ["imageUploadUtil.js", "manage.js"],
+                        id: storageId,
+                        auth: auth,
+                    });
+                }
+            );
+
         });
-    } catch (error) {
-        console.error("Error fetching storage:", error);
-        res.status(500).json({ error: "Internal server error" });
-    } finally {
-        await client.end();
-    }
+
+    });
+
 });
 
 // Route for profile page
@@ -215,7 +232,6 @@ app.get("/profile", async function (req, res) {
     const userId = req.session.userId;
     if (!userId) {
         return res.redirect("/login");
-
     }
 
     const client = new pg.Client(config);
@@ -235,7 +251,7 @@ app.get("/profile", async function (req, res) {
 
         res.render("profile", {
             userInfo,
-            stylesheets: ["browse.css","reviews.css", "profile.css"],
+            stylesheets: ["browse.css", "reviews.css", "profile.css"],
             scripts: ["profile.js"],
         });
     } catch (error) {
@@ -247,6 +263,7 @@ app.get("/profile", async function (req, res) {
 });
 
 // Route for create new fridge/pantry page
+
 app.get("/storage/createnew", (req, res) => {
     res.render("create_new", {
         stylesheets: ["create_new.css"],
@@ -257,16 +274,19 @@ app.get("/storage/createnew", (req, res) => {
 ///route for reviews
 app.get("/reviews/:storageId", function (req, res) {
     const storageId = req.params.storageId;
-    res.render("reviews", {
-        stylesheets: ["reviews.css", "contents.css", "addreview.css"],
-        scripts: ["reviews.js"],
-        other: [
-            `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    authorization.isAuthorized(storageId, req.session.userId).then(auth => {
+        res.render("reviews", {
+            stylesheets: ["reviews.css", "contents.css", "addreview.css"],
+            scripts: ["reviews.js"],
+            other: [
+                `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">`,
-            `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>`,
-        ],
-        id: storageId,
-    });
+                `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>`,
+            ],
+            id: storageId,
+            auth: auth,
+        });
+    })
 });
 
 app.post("/reviews/:storageId", async (req, res) => {
